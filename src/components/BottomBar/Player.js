626c1c8fef36a09eaad4572d4262cc3e517030bf
Sultan Bozkurt <sbozkurt@nuevo.com.tr>
2022-05-20 17:23:17 +0300

    firs commit

--- src/components/BottomBar/Player.js
+++ src/components/BottomBar/Player.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { useState } from 'react';
+
 import { Icon } from 'Icons';
-import { Range, getTrackBackground } from "react-range";
 import {useAudio} from 'react-use';
 import { secondsToTime } from 'utils';
+import CustomRange from 'components/CustomRange';
 
 
 function Player() {
-    const STEP = 0.1;
-    const MIN = 0;
+
     const [audio, state, controls, ref] = useAudio({
         src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
  
@@ -27,69 +25,38 @@ function Player() {
                 <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
                     <Icon size={16} name="shuffle"/>
                 </button>
+
                 <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
                 <Icon size={16} name="playerPrev"/>
                 </button>
-                <button className='w-8 h-8 flex items-center justify-center bg-white text-black rounded-full hover:scale-[1.06]' onClick={controls.play}>
-                <Icon size={16} name="play"/>
+
+                <button className='w-8 h-8 flex items-center justify-center bg-white text-black rounded-full hover:scale-[1.06]'
+                 onClick={ controls[state?.playing ? 'pause' : 'play']}>
+                <Icon size={16} name={state?.playing ? 'pause' : 'play'}/>
                 </button>
+
                 <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
                 <Icon size={16} name="playerNext"/>
                 </button>
+
                 <button className='w-8 h-8 flex items-center justify-center text-white text-opacity-70 hover:text-opacity-100'>
                 <Icon size={16} name="repeat"/>
                 </button>
+
             </div>
             <div className='w-full flex items-center gap-x-2'>
                 {audio}
                 <div className='text-[0.688rem] text-white text-opacity-70' >
             {secondsToTime(state?.time)}
         </div>
-                    <Range
-          values={[state?.time]}
-          step={STEP}
-          min={MIN}
-          max={state?.duration || 1}
-          onChange={(values) => controls.seek(values[0])}
-          renderTrack={({ props, children }) => (
-            <div
-              onMouseDown={props.onMouseDown}
-              onTouchStart={props.onTouchStart}
-              className="w-full h-7 flex group"
-              style={props.style}
-            >
-              <div
-                ref={props.ref}
-                className="h-1 w-full rounded-md self-center"
-                style={{
-               
-                  background: getTrackBackground({
-                    values: [state?.time],
-                    colors: ["#1db954", "#535353"],
-                    min: MIN,
-                    max: state?.duration || 1
-                  }),
-                 
-                }}
-              >
-                {children}
-              </div>
-            </div>
-          )}
-          renderThumb={({ props, isDragged }) => (
-            <div
-              {...props}
-              className={`h-3 w-3 rounded-full bg-white ${! isDragged ? 'opacity-0' : ''} group-hover:opacity-100`}
-              style={{
-                ...props.style,
-               
-                boxShadow: "0 2px 4px 0 rgba(0 0 0 / 50%)"
-              }}
-            >
-              
-            </div>
-          )}
+        <CustomRange
+        step={0.1}
+        min={0}
+        max={state?.duration || 1}
+        value={state?.time}
+        onChange={}
         />
+                
         <div className='text-[0.688rem] text-white text-opacity-70' >
             {secondsToTime(state?.duration)}
         </div>
